cbe5056eb0dd9231cfe2ef5491e49c7bc32950d6
amarquez914 <aaem914.dev@gmail.com>
2021-02-22 13:32:19 -0500

    Final touches to the Model project

--- DGModels/DancingGoatQuery.cs
+++ DGModels/DancingGoatQuery.cs
@@ -1,4 +1,7 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
+using DGModels.Types;
+using System;
 
 namespace DGModels
 {
@@ -6,7 +9,27 @@ namespace DGModels
     {
         public DancingGoatQuery(DancingGoatData data)
         {
+            Name = "Query";
+
+            Func<IResolveFieldContext, string, object> GetCoffeeFunc = (context, id) => data.GetCoffeeByIdAsync(int.Parse(id));
+
+            Func<IResolveFieldContext, string, object> GetBrewerFunc = (context, id) => data.GetBrewerByIdAsync(int.Parse(id));
+
+            FieldDelegate<CoffeeModel>(
+                  "coffee",
+                  arguments: new QueryArguments(
+                      new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "id of the coffee" }
+                      ),
+                  resolve: GetCoffeeFunc
+                  );
+
+            FieldDelegate<BrewerModel>(
+                "brewer",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "id", Description = "id of the brewer" }
+            ),
+            resolve: GetBrewerFunc
+            );
         }
-            
+
     }
 }
