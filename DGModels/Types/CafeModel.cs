a3b1355480ff9140aae03967306ad69e26bc26c0
amarquez914 <aaem914.dev@gmail.com>
2021-02-22 14:31:49 -0500

    adding the core files

---
+++ DGModels/Types/CafeModel.cs
@@ -0,0 +1,23 @@
+﻿using GraphQL.Types;
+
+namespace DGModels.Types
+{
+    public class CafeModel : ObjectGraphType<DG_Cafe>
+    {
+        public CafeModel()
+        {
+            Name = "Cafe";
+
+            Field(h => h.Id).Description("The id of the coffee product");
+            Field(h => h.Name, nullable: true).Description("The name of coffee");
+            Field(h => h.Street).Description("");
+            Field(h => h.City).Description("");
+            Field(h => h.Country).Description("");
+            Field(h => h.ZipCode).Description("");
+            Field(h => h.Phone).Description("");
+            Field(h => h.Email).Description("");
+            Field(h => h.IsRoastery).Description("");
+            Field(h => h.IsPartner).Description("");
+        }
+    }
+}
